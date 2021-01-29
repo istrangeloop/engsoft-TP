f3224d8d1712df105201ef0ff93bf72893948311
Ingrid Spangler <ingridespangler@gmail.com>
2021-01-29 18:31:06 -0300

    tema

---
+++ dietario/src/theme.js
@@ -0,0 +1,40 @@
+ 
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  root: {
+    width: '100%',
+    height: '100%',
+    color: 'white',
+    background: 'linear-gradient(45deg, #8E6BFB 30%, #8FFE53 90%)',
+    fontFamily: "Roboto",
+  },
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '10px 30px',
+},
+  spacing: 2,
+});
+
+export default theme;
