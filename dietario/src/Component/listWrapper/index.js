67c23bf674fbdfe42c73861caef6c8573ccd4a57
henrany <tmkhenry@gmail.com>
2021-01-30 15:02:39 -0300

    list wrapper

--- dietario/src/Component/listWrapper/index.js
+++ dietario/src/Component/listWrapper/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import {Typography, Container,Grid} from '@material-ui/core';
+import React,{useState,useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from '@material-ui/core';
+import * as foodNutrients from '../../foodNutrients';
+import List from './table';
 
 const ListWrapper = () =>{
+  const [foods,setFoods] = useState(null);
+
+  const handelFetch = async () =>{
+    await foodNutrients.init();
+    const data = foodNutrients.getFoods();
+    setFoods(data);
+  }
+
+  useEffect(()=>{
+    handelFetch();
+  },[]);
+
     return (
       <>
-        <Typography>this is a list Wrapper</Typography>
+        <List
+          items = {foods}
+        />
+        <Button variant="contained" color="primary"><Link to={{pathname:'/'}}> voltar</Link></Button>
       </>
     );
 }
